fd526636a3ca9c491516342f4ed17545f2200d98
Christopher Dunn <cdunn2001@gmail.com>
2018-05-24 12:48:19 -0500

    Make setup.py work w/ py3

--- pypeflow/__init__.py
+++ pypeflow/__init__.py
@@ -1,5 +1,6 @@
 try:
     import sys, pkg_resources
+    version = pkg_resources.get_distribution('pypeflow')
     sys.stderr.write('{}\n'.format(pkg_resources.get_distribution('pypeflow')))
 except Exception:
-    pass
+    version = '2.0.3' # should match setup.py
